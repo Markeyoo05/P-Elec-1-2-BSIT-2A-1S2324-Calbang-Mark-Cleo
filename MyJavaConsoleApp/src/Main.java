41c25b638cb17896f0d695144c277fda4c98f246
Mark Cleo Calbang <markcleocalbang22@gmail.com>
2023-09-27 23:03:09 +0800

    TryIfokey

---
+++ MyJavaConsoleApp/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		double number1 = input.nextDouble();
+
+		System.out.print("Enter second number: ");
+		double number2 = input.nextDouble();
+
+		double product = number1 * number2;
+		System.out.printf("The product of both numbers is: %f", product);
+	}
+}
